5e15b9e02365e096dc850d0d6e9aa8eb43cb9c65
rk-ramya <116821018+rk-ramya@users.noreply.github.com>
2022-10-28 22:49:37 +0530

    Add files via upload

--- Question1.java
+++ Question1.java
@@ -8,7 +8,7 @@ public class Question1 {
 		for(int i=0;i<n;i++) {
 			for(int j=0;j<n;j++) {
 				if(i==0||i==n-1||j==n/2)
-					System.out.print("# ");
+					System.out.print("I ");
 				else
 					System.out.print("  ");
 			}			 	
@@ -18,7 +18,7 @@ public class Question1 {
 		
 				for(int j=0;j<n;j++) {
 					if(j==0||j==n-1||i==j)
-						System.out.print("# ");
+						System.out.print("N ");
 					else
 						System.out.print("  ");
 				}
@@ -29,7 +29,7 @@ public class Question1 {
 		
 				for(int j=0;j<n;j++) {
 					if(i==0||j==0||i==n-1||i==(n-1)/2)
-						System.out.print("# ");
+						System.out.print("E ");
 					else
 						System.out.print("  ");
 				}
@@ -40,7 +40,7 @@ public class Question1 {
 		
 				for(int j=0;j<n;j++) {
 					if((j==0&&i!=n-1)||(j==n-1&&i!=n-1)||i==n-1)
-						System.out.print("# ");
+						System.out.print("U ");
 					else
 						System.out.print("  ");
 				}
@@ -53,7 +53,7 @@ public class Question1 {
 			for(int j=0;j<n;j++) {
 							if((j==0|| i-j==(n-1)/2)||
 									i<=n/2&&j==n/2||i==0&&j<n/2||i==n/2&&j<n/2)
-								System.out.print("# ");
+								System.out.print("R ");
 							else
 								System.out.print("  ");
 						}
@@ -64,7 +64,7 @@ public class Question1 {
 		
 				for(int j=0;j<n;j++) {
 					if(i==0||j==0||i==n-1||j==(n-1))
-						System.out.print("# ");
+						System.out.print("O ");
 					else
 						System.out.print("  ");
 				}
@@ -75,7 +75,7 @@ public class Question1 {
 
 				for(int j=0;j<n;j++) {
 					if(j==0||j==(n-1)||i==j)
-						System.out.print("# ");
+						System.out.print("N ");
 					else
 						System.out.print("  ");
 				}
